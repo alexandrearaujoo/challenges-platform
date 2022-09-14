eedbd093fc89473e004eb4560e4fa4c68b77c8d2
Alexandre Araujo <alexandre01393@gmail.com>
2022-09-14 17:16:24 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { rgba } from "polished";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 2px ${({ theme }) => rgba(theme.primary, 0.2)}
+    }
+  }
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text}
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  ul {
+    list-style: none
+  }
+`;
